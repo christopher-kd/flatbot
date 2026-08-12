0c3dc301121ec7a80d26812601467f4c92de4a44
Christopher Käding <99ctl.kd@gmail.com>
2026-08-12 22:11:48 +0200

    Remove search messages

--- src/core/banner.ts
+++ src/core/banner.ts
@@ -1,13 +1,12 @@
 import { blue, cyanBright, italic } from "colorette"
 import figlet from "figlet"
 
-export async function printBanner(message: string): Promise<void> {
+export async function printBanner(): Promise<void> {
 	const banner = await figlet.text("FLATBOT", {
 		font: "Tmplr",
 	})
 	const output = [
-		`\n${blue(banner).trim().split("\n").slice(0, -1).join("\n")}`,
-		`${italic(cyanBright(message))}\n`,
+		`\n${blue(banner).trim().split("\n").slice(0, -1).join("\n")}\n`
 	]
-	console.log(output.join("\n"))
+  console.log(output.join("\n"))
 }
